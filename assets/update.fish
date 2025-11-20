cb04464491f7c98f9742502a712f21485f9d878e
ahaoboy <504595380@qq.com>
2025-11-20 17:16:16 +0800

    svg

---
+++ assets/update.fish
@@ -0,0 +1,5 @@
+ansi2 cmd "neofetch" -f ans > fish.ans
+ansi2 cmd "crash install" -f ans >> fish.ans
+ansi2 cmd "crash update-url" -f ans >> fish.ans
+ansi2 cmd "crash start" -f ans >> fish.ans
+ansi2 cmd "curl google.com" -f ans >> fish.ans
