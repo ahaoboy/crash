5e1018c81a7a904d44915383af2b6bc11e61e53e
ahaoboy <504595380@qq.com>
2025-11-22 21:39:54 +0800

    doc

--- assets/update.fish
+++ assets/update.fish
@@ -1,5 +1,2 @@
-ansi2 cmd "neofetch" -f ans > fish.ans
-ansi2 cmd "crash install" -f ans >> fish.ans
-ansi2 cmd "crash update-url" -f ans >> fish.ans
-ansi2 cmd "crash start" -f ans >> fish.ans
-ansi2 cmd "curl google.com" -f ans >> fish.ans
+ansi2 cmd -c "neofetch" -c "crash install" -c "crash update-url" -c "crash start" -c "curl google.com" -f ans > assets/crash.ans
+ansi2 assets/crash.ans > assets/crash.svg
